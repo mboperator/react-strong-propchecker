99c0ade82c2e2e1a5a28d4417ff7ea8caa1468f7
Marcus Bernales <mboperator@gmail.com>
2016-08-03 21:30:57 -0700

    Added strong validator

---
+++ src/validateStrong.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2013-2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+import invariant from 'invariant';
+import warning from './warning';
+
+var loggedTypeFailures = {};
+
+var validateStrong = (propTypes, props, className) => {
+  let sanitized = {};
+  for (var propName in propTypes) {
+    if (propTypes.hasOwnProperty(propName)) {
+      var error;
+      // Prop type validation may throw. In case they do, we don't want to
+      // fail the render phase where it didn't fail before. So we log it.
+      // After these have been cleaned up, we'll let them throw.
+      try {
+        // This is intentionally an invariant that gets caught. It's the same
+        // behavior as without this statement except with a better message.
+        invariant(
+          typeof propTypes[propName] === 'function',
+          '%s: %s type `%s` is invalid; it must be a function, usually from ' +
+          'PropTypes.',
+          className,
+          'attributes',
+          propName
+        );
+
+        error = propTypes[propName](props, propName, className, 'prop');
+      } catch (ex) {
+        error = ex;
+      }
+      if (error instanceof Error && !(error.message in loggedTypeFailures)) {
+        // Only monitor this failure once because there tends to be a lot of the
+        // same error.
+        loggedTypeFailures[error.message] = true;
+        warning(false, 'Failed propType: ' + error.message);
+      }
+      sanitized[propName] = props[propName];
+    }
+  }
+  return sanitized;
+};
+
+export default validateStrong;
